85fe8c4453cb6f700cbc53414d68d861b32f320d
Toshiaki Aoike <aoike@cloudpack.jp>
2019-02-12 19:11:15 +0900

    modify

--- index.py
+++ index.py
@@ -3,14 +3,13 @@ import json
 
 def lambda_handler(event, context):
     print(event)
-    headers = {
-        "access-control-allow-headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token ",
-        "access-control-allow-methods": "GET,OPTIONS",
-        "access-control-allow-origin": "*"
-    }
 
     return {
         "statusCode": 200, 
-        "headers": headers,
+        "headers": {
+            "access-control-allow-headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token ",
+            "access-control-allow-methods": "GET,OPTIONS",
+            "access-control-allow-origin": "*"
+        },
         "body": '{"result":"Hello from Lambda!!!"}'
     }
